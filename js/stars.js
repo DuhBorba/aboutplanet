884190f0b64c11236a0469a3f9d057eb38c5174e
Eduardo Luis Borba <eduardoborba3@gmail.com>
2021-01-26 17:38:49 -0300

    Speed stars feito

--- js/stars.js
+++ js/stars.js
@@ -6,16 +6,12 @@ function stars() {
     let star = document.createElement("i");
     let x = Math.floor(Math.random() * window.innerWidth);
     let y = Math.floor(Math.random() * window.innerHeight);
-    let duration = Math.random() * 10;
     let size = Math.random() * 2;
 
     star.style.left = x + 'px';
     star.style.top = y + 'px';
     star.style.width = 1 + size + 'px';
     star.style.height = 1 + size + 'px';
-
-    star.style.animationDuration = 5 + duration + 's';
-    star.style.animationDelay = duration + 's';
 
     scene.appendChild(star);
     i++;
