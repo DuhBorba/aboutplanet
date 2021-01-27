045e950a926775808af3aaced9560a7b84ce70cb
Eduardo Luis Borba <eduardoborba3@gmail.com>
2021-01-27 17:40:52 -0300

    Orbita feito

--- js/stars-speed.js
+++ js/stars-speed.js
@@ -25,56 +25,30 @@
 // starsSpeed();
 
 
-// function starsSpeed() {
-//   let count = 500;
-//   let scene_stars_speed = document.querySelector('.scene-stars-speed');
-//   let i = 0;
-//   while (i < count) {
-//     let star = document.createElement("i");
-//     let x = Math.floor(Math.random() * window.innerWidth);
-//     let duration = Math.random() * 1;
-//     let size = Math.random() * 2;
-//     let h = Math.random() * 100;
-
-//     star.style.left = x + 'px';
-//     star.style.width = 1 + 'px';
-//     // star.style.height = 50 + h + 'px';
-//     // star.style.height = 1 + size + 'px';
-//     star.style.width = 1 + size + 'px';
-//     star.style.height = 1 + size + 'px';
-
-//     star.style.animationDuration = duration + 's';
-//     // star.style.animationDelay = duration + 's';
-    
-
-//     scene_stars_speed.appendChild(star);
-//     i++;
-//   }
-// }
-// starsSpeed();
-
-
-function stars() {
+function starsSpeed() {
   let count = 500;
-  let scene = document.querySelector('.scene');
+  let scene_stars_speed = document.querySelector('.scene-stars-speed');
   let i = 0;
   while (i < count) {
     let star = document.createElement("i");
     let x = Math.floor(Math.random() * window.innerWidth);
-    let y = Math.floor(Math.random() * window.innerHeight);
-    let duration = Math.random() * 10;
+    let duration = Math.random() * 1;
     let size = Math.random() * 2;
+    let h = Math.random() * 100;
 
     star.style.left = x + 'px';
-    star.style.top = y + 'px';
+    star.style.width = 1 + 'px';
+    // star.style.height = 50 + h + 'px';
+    // star.style.height = 1 + size + 'px';
     star.style.width = 1 + size + 'px';
     star.style.height = 1 + size + 'px';
 
-    star.style.animationDuration = 5 + duration + 's';
+    star.style.animationDuration = duration + 's';
     // star.style.animationDelay = duration + 's';
+    
 
-    scene.appendChild(star);
+    scene_stars_speed.appendChild(star);
     i++;
   }
 }
-stars();
+starsSpeed();
